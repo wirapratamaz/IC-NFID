811758f754d6b250315112ba4122eec9d28116b7
Seva Rybakov <jumpincode@gmail.com>
2024-02-12 16:30:37 +0800

    fix map to take 100%

--- src/components/hood-map.tsx
+++ src/components/hood-map.tsx
@@ -5,14 +5,14 @@ import Map from "react-map-gl";
 
 export function HoodMap() {
   return (
-    <div className="w-[100vh] h-[100vh]">
+    <div className="w-[100vw] h-[100vh]">
       <Map
         style={{ width: "100%", height: "100%" }}
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
         mapStyle="mapbox://styles/mapbox/streets-v12"
         initialViewState={{
-          latitude: 35.668641,
-          longitude: 139.750567,
+          latitude: -8.635769422572857,
+          longitude: 115.13914782040987,
           zoom: 15,
         }}
         maxZoom={20}
