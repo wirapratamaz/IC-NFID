a827da08c7b173461e18aa891b40bdfd9221d6ca
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-02-19 23:12:43 +0800

    feat: add provider connect to juno

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -9,43 +9,10 @@ import { useEffect, useState } from "react";
 import {
   type Doc,
   initJuno,
-  setDoc,
-  authSubscribe,
-  User,
-  getDoc,
 } from "@junobuild/core-peer";
-import { authState } from "@/atoms/auth";
 import { ThemeProvider } from "next-themes";
-import { Profile, fetchProfile, profileState } from "@/atoms/profile";
-import { useRouter } from "next/navigation";
 
 function JunoAuthProvider({ children }: ProvidersProps) {
-  const [user, setUser] = useRecoilState(authState);
-  const [profile, setProfile] = useRecoilState<Profile | null>(profileState);
-  const router = useRouter();
-
-  useEffect(() => {
-    async function fetchProfileAndRedirect(u: User) {
-      const _profile = await fetchProfile(u.key);
-
-      if (!_profile) {
-        router.push("/profile?new=true");
-      } else {
-        setProfile(_profile.data);
-      }
-    }
-    const unsubscribe = authSubscribe((user: User | null) => {
-      setUser(user);
-      console.log("got user", user);
-      if (user) {
-        fetchProfileAndRedirect(user);
-      } else {
-        setProfile(null);
-      }
-    });
-
-    return unsubscribe;
-  }, []);
 
   useEffect(() => {});
 
